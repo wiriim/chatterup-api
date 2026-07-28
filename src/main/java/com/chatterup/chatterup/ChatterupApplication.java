8ea4d16c0436e75d7aff0deff77458677650f60f
William Halim <51820876+wiriim@users.noreply.github.com>
2026-07-28 11:28:32 +0700

    feat: downgrade spring to 3.5.6

--- src/main/java/com/chatterup/chatterup/ChatterupApplication.java
+++ src/main/java/com/chatterup/chatterup/ChatterupApplication.java
@@ -2,12 +2,15 @@ package com.chatterup.chatterup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup;
 
 @SpringBootApplication
 public class ChatterupApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ChatterupApplication.class, args);
+		SpringApplication app = new SpringApplication(ChatterupApplication.class);
+		app.setApplicationStartup(new BufferingApplicationStartup(2048));
+		app.run(args);
 	}
 
 }
